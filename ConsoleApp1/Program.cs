41beb00716dbcc4162164d45aff4314583cff1bb
PC <fontes.leonardo@aluno.ifsp.edu.br>
2023-08-15 13:03:58 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Random numAleatorio = new Random();
+int numAleatorioInteiro = numAleatorio.Next(1, 101);
+
+do
+{
+    Console.WriteLine("Digite um número de 1 a 100");
+    int numDigitado = int.Parse(Console.ReadLine()!);
+
+    if (numDigitado < numAleatorioInteiro)
+    {
+        Console.WriteLine("Adicione pontos!");
+    }
+    else if (numDigitado > numAleatorioInteiro)
+    {
+        Console.WriteLine("Subtraia pontos!");
+    }
+    else
+    {
+        Console.WriteLine("Parabéns, você venceu!");
+        break;
+    }
+} while (true);
+    
+
+
+
